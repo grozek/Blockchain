4351e5c22d744bcb4ec66252d77389df189afa9c
grozek <roznawsk@grinnell.edu>
2023-11-06 20:19:06 -0600

    Fully functional class

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,4 @@
+import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -5,13 +6,22 @@ public class Block {
   int block;
   int amtTransferred;
   Hash prevHash;
-  Long nonce;
+  long nonce;
   Hash hash;
   
   public Block(int num, int amount, Hash prevHash) {
     this.block = num;
     this.amtTransferred = amount;
     this.prevHash = prevHash;
+
+    // if statement that this is block 0 does sth diff
+    int nonce = 1;
+    int sizeOfNonce = 4;
+    byte[] nonceByteArr;
+
+    while (!this.hash.isValid()) {
+      nonceByteArr = ByteBuffer.allocate(sizeOfNonce).putInt(nonce).array();
+    }
 
 
   } // Block(int, int, Hash)
@@ -45,11 +55,22 @@ public class Block {
   } // toString()
 
    
-  public static byte[] calculateHash(String msg) throws NoSuchAlgorithmException {
+  public static byte[] calculateHash(int block, int amtTransferred, Hash prevHash, long nonce) throws NoSuchAlgorithmException {
     MessageDigest md = MessageDigest.getInstance("sha-256");
-    md.update(msg.getBytes());
-    byte[] hash = md.digest();
+    if(block == 0){
+      md.update((Integer.toString(block)).getBytes()); // update w/ block num
+      md.update((Integer.toString(amtTransferred)).getBytes()); // update w/ amtTransferred
+      md.update((Long.toString(nonce)).getBytes()); // update w/ nonce
+    }
+    else{
+    md.update((Integer.toString(block)).getBytes()); // update w/ block num
+    md.update((Integer.toString(amtTransferred)).getBytes()); // update w/ amtTransferred
+    md.update((prevHash.toString()).getBytes()); // update w/ prevHash
+    md.update((Long.toString(nonce)).getBytes()); // update w/ nonce
+    }
+    byte[] hash = md.digest(); 
     return hash;
-  } // calculateHash(String)
+   // calculateHash(String)
 
 } // class Block
+}
