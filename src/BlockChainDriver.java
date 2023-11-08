a2fc4e29e62050de72b446737a3c20e14c1a1bc4
grozek <roznawsk@grinnell.edu>
2023-11-08 16:36:40 -0600

    technically final draft of the interface

--- src/BlockChainDriver.java
+++ src/BlockChainDriver.java
@@ -12,12 +12,16 @@ public class BlockChainDriver {
     do {
       pen.printf(aBlockChain.toString() + "\n"); 
       pen.printf("Command? ");
-      input = scan.nextLine();
+      input = scan.next();
 
       switch(input) {
         case "mine" :
-
+          pen.printf("\n" + "Amount transferred? ");
+          input = scan.next();
+          Block tempBlock = aBlockChain.mine(Integer.valueOf(input));
+          pen.printf("\n" + "amount = " + input + ", " + "nonce = " + tempBlock.nonce);
           break;
+
         case "append":
           pen.printf("\n" + "Amount transferred? ");
           input = scan.next();
@@ -25,22 +29,36 @@ public class BlockChainDriver {
           pen.printf("\n" + "Nonce? ");
           input = scan.next();
           int nonce = Integer.valueOf(input);
-
-          //Block newBlock = new Block();
-
-          
+          Block newBlock = new Block (aBlockChain.last.data.block+1, amtTransferred, aBlockChain.last.data.hash, nonce);
+          aBlockChain.append(newBlock);
           break;
+
         case "remove" :
-
+          aBlockChain.removeLast();
           break;
+
         case "check":
-
+          if (aBlockChain.isValidBlockChain()){
+            pen.printf("\n" + "Chain is valid!");
+          }else{
+            pen.printf("\n" + "Chain is invalid!");
+          }
           break;
+
         case "report":
-
+          pen.printf("\n");
+          aBlockChain.printBalances();
           break;
-        case "help":
 
+        case "help":
+          pen.printf("\n" + "Valid Commands" + "\n" +     
+                    " \t mine: discovers the nonce for a given transaction" +
+                    " \t append: appends a new block onto the end of the chain" +
+                    " \t remove: removes the last block from the end of the chain" +
+                    " \t check: checks that the block chain is valid" +
+                    " \t report: reports the balances of Alexis and Blake" +
+                    " \t help: prints this list of commands" +
+                    " \t quit: quits the program" + "\n");
           break;
       }
     } while(input != "quit");
